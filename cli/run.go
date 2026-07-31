f7b36731cad9c5e0fb715fe79b4c1f5a648749f2
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-07-30 19:48:46 -0700

    Making id a required parameter for reveal
    Using SplitSeq in run

--- cli/run.go
+++ cli/run.go
@@ -66,7 +66,7 @@ func RunHandler(u *url.URL) error {
 	}
 
 	if q.Has(vangogh_integration.UrlArgParameter) {
-		for _, arg := range strings.Split(q.Get(vangogh_integration.UrlArgParameter), ",") {
+		for arg := range strings.SplitSeq(q.Get(vangogh_integration.UrlArgParameter), ",") {
 			et.args = append(et.args, strings.TrimPrefix(arg, "\\"))
 		}
 	}
